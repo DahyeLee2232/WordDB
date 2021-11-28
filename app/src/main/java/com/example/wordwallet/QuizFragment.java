ebb98b653a339ab4a29d688e8716af124101f09e
DahyeLee2232 <ida@soongsil.ac.kr>
2021-11-29 03:45:35 +0900

    fragment experiment

--- app/src/main/java/com/example/wordwallet/QuizFragment.java
+++ app/src/main/java/com/example/wordwallet/QuizFragment.java
@@ -11,7 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_myword, container, false);
+        return inflater.inflate(R.layout.fragment_quiz, container, false);
     }
 
 
