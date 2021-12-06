95fe8a4d586205aa2ec9c5902566214f9e4cb634
yunk-k <hamndo@naver.com>
2021-12-06 20:04:57 +0900

    Quiz Fragment.java, wordListSelect.java, check_wordlist.xml

--- app/src/main/java/com/example/wordwallet/QuizFragment.java
+++ app/src/main/java/com/example/wordwallet/QuizFragment.java
@@ -1,21 +1,60 @@
 package com.example.wordwallet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class QuizFragment extends Fragment {
+
+    Button Q1, Q2, Q3;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_quiz, container, false);
+
+        Q1 = (Button) view.findViewById(R.id.Q1);
+        Q2 = (Button) view.findViewById(R.id.Q2);
+        Q3 = (Button) view.findViewById(R.id.Q3);
+
+        Q1.setOnClickListener(new View.OnClickListener() { // 첫번쨰 Quiz 누르면 범위 설정으로 넘어감
+
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent intent = new Intent(WWmainActivity, wordListSelect.class);
+                intent.putExtra("QuizNumber", 1);
+                startActivity(intent);
+                 */
+            }
+        });
+
+        Q2.setOnClickListener(new View.OnClickListener() { // 두번째 Quiz 누르면 범위 설정으로 넘어감
+
+            @Override
+            public void onClick(View v) {
+                /*
+                Intent intent = new Intent(QuizFragment.this, wordListSelect.class);
+                intent.putExtra("QuizNumber", 2);
+                startActivity(intent);
+                 */
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_quiz, container, false);
     }
-
 
     public void onListItemClick(RecyclerView r, View v, int position, long id){
         // super.onListItemClick(r, v, position, id);
     }
+
 }
---
+++ app/src/main/java/com/example/wordwallet/wordListSelect.java
@@ -0,0 +1,184 @@
+package com.example.wordwallet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class wordListSelect extends AppCompatActivity {
+
+    Intent intent;
+    int QuizNum;
+    Button selected;
+    ListView listView;
+    ArrayList<String> showList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_list_select);
+
+        selected = findViewById(R.id.selected);
+        intent = getIntent();
+        QuizNum = intent.getIntExtra("QuizNumber", 0);
+
+        DBHelper helper = new DBHelper(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("select * from wordList", null); // where listnumber exists ( 범위 선택 받은 것의 list)
+
+
+        for (int j = 0; j < cursor.getCount(); j++) {
+            //n번째 단어의 스키마
+            cursor.moveToNext();
+            showList.add(cursor.getString(1));
+        }
+
+
+
+
+        selected.setOnClickListener(new View.OnClickListener() { // 저장 버튼 눌리면 선택된 단어 리스트 배열로 putExtra 해 줌
+
+            @Override
+            public void onClick(View v) {
+                if (QuizNum == 1) {
+                    Intent intent1 = new Intent(wordListSelect.this, Quiz1.class);
+                    //putExtra 단어장 리스트
+
+
+                    startActivity(intent1);
+                }
+                if (QuizNum == 2) {
+                    Intent intent1 = new Intent(wordListSelect.this, Quiz2.class);
+                    //putExtra 단어장 리스트
+                    startActivity(intent1);
+                }
+            }
+
+        });
+
+        //-----------------------------------------------------------------------------------여기서부터 서치한 Custom Adaptor 코드
+
+        MyAdapter adapter = new MyAdapter(showList);
+
+        listView = findViewById(R.id.select_list);
+        listView.setAdapter(adapter);
+    }
+
+
+    class MyAdapter extends BaseAdapter {
+
+        boolean[] checkBoxState;
+
+        ArrayList<String> myList;
+
+        public MyAdapter(ArrayList<String> myList) {
+
+            this.myList = myList;
+            checkBoxState = new boolean[myList.size()];
+        }
+
+        // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
+        @Override
+        public int getCount() {
+
+            return myList.size();
+        }
+
+        // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            final Context context = parent.getContext();
+
+            // 커스텀뷰 참조 획득.
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.check_wordlist, parent, false);
+            }
+
+            // position에 위치한 데이터 획득
+            String listViewItem = myList.get(position);
+
+            final LinearLayout lin = convertView.findViewById(R.id.lin);
+            final CheckBox checkBox = convertView.findViewById(R.id.checkBox);
+
+            // 아이템 내 각 위젯에 데이터 반영
+            checkBox.setText(listViewItem);
+
+            checkBox.setOnClickListener(new CheckBox.OnClickListener() {
+
+                public void onClick(View v) {
+
+                    if (checkBoxState[position] == true) {
+                        checkBoxState[position] = false;
+                    } else {
+                        checkBoxState[position] = true;
+                    }
+                }
+            });
+
+            lin.setOnClickListener(new Button.OnClickListener() {
+
+                public void onClick(View v) {
+                    //리스트 클릭시 체크박스에 체크
+                    if (checkBox.isChecked() == false) {
+                        checkBox.setChecked(true);
+                        notifyDataSetChanged(); //새로고침 같은 기능
+                        checkBoxState[position] = true;
+                    }
+                    //한번 더 누르면 체크 풀림
+                    else {
+                        checkBox.setChecked(false);
+                        notifyDataSetChanged();
+                        checkBoxState[position] = false;
+                    }
+                }
+            });
+
+            //여기서 checkBoxState가 true 인 아이들의 체크박스를 체크해주는 기능을 넣어주는 것이 중요합니다.
+            //이러한 방식을 적용할 경우 converView로 뷰를 재사용하여 아래로 내려왔다 올라올 경우 다른 곳에 체크가 되거나 체크가 해제되는 것과 같은 문제를 해결할 수 있게 됩니다!
+            if (checkBoxState[position] == true) {
+                checkBox.setChecked(true);
+            } else {
+                checkBox.setChecked(false);
+            }
+
+            return convertView;
+        }
+
+        // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+        @Override
+        public long getItemId(int position) {
+
+            return position;
+        }
+
+        // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+        @Override
+        public Object getItem(int position) {
+
+            return myList.get(position);
+        }
+
+    }
+
+}
+
+
+
