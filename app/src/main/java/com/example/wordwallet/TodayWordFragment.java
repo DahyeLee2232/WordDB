d98800f85dff32f26acfdd730b7fd138d23a5bc9
DahyeLee2232 <ida@soongsil.ac.kr>
2021-12-08 10:39:50 +0900

    merge failed

--- app/src/main/java/com/example/wordwallet/TodayWordFragment.java
+++ app/src/main/java/com/example/wordwallet/TodayWordFragment.java
@@ -6,66 +6,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ExpandableListView;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
+
 public class TodayWordFragment extends Fragment {
-    private RecyclerView recyclerView;
-    private TodayListAdapter adapter;
-    private RecyclerView.LayoutManager layoutManager;
 
-    private ArrayList<WordListClass> list;
+    Button addBtn;
+    ExpandableListView listView;
+    TodayExpandableListAdapter adapter;
+    ArrayList<ParentItem>  wordLists;        //단어장 이름
+    ArrayList<ArrayList<ChildItem>> wordList;        //단어장 리스트
 
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         makeList();
+        return inflater.inflate(R.layout.fragment_todayword, container, false);
     }
 
-    private void makeList(){
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addBtn = view.findViewById(R.id.add_Btn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { }
+        });
+
+        listView = view.findViewById(R.id.expandable_wordlist);
+        listView.setGroupIndicator(null);
+        adapter = new TodayExpandableListAdapter(getContext(), wordLists, wordList);
+        listView.setAdapter(adapter);
+        //단어장 펼침 닫힘
+
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                parent.smoothScrollToPosition(groupPosition);
+                return false;
+            }
+        });
+    }
+
+    private void makeList() {
         DBHelper helper = new DBHelper(getContext());
         SQLiteDatabase db = helper.getWritableDatabase();
-        list = new ArrayList<WordListClass>();
-        int id = -1;
-        String listName = "";
-        ArrayList<WordItem> wordList = new ArrayList<>();
+        wordLists = new ArrayList<>();
+        wordList = new ArrayList<>();
+        //db 값 들어올 변수들
+        ParentItem p;
+        ArrayList<ChildItem> l;
 
         Cursor listCursor = db.rawQuery("select _id, name from wordlist where day_my=0", null);
 
-        while(listCursor.moveToNext()){
-            id = listCursor.getInt(0);
-            listName = listCursor.getString(1);
+        while (listCursor.moveToNext()) {
+            //단어장 리스트 번호와 이름을 읽어온다
+            p = new ParentItem(listCursor.getInt(0), listCursor.getString(1));
 
-            Cursor wordCursor = db.rawQuery("select word, meaning, imageLink from word where listnumber=?", new String[]{String.valueOf(id)});
+            //단어장 하나의 단어들을 읽어온다
+            Cursor wordCursor = db.rawQuery("select _id, word, meaning, imageLink from word where listnumber="+p.id_pk, null);
+
+            //단어장에 단어 하나 씩 저장
+            l = new ArrayList<>();
             while(wordCursor.moveToNext()){
-                wordList.add(new WordItem(wordCursor.getString(0), wordCursor.getString(1), wordCursor.getString(2)));
+                l.add(new ChildItem(wordCursor.getInt(0), wordCursor.getString(1), wordCursor.getString(2), wordCursor.getString(3)));
             }
-            list.add(new WordListClass(listName, wordList));
+            wordLists.add(p);
+            wordList.add(l);
         }
         db.close();
     }
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_todayword, container, false);
-
-
-
-        recyclerView = (RecyclerView) view.findViewById(R.id.todayword_recycler);
-
-        recyclerView.setHasFixedSize(true);
-
-        layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new TodayListAdapter(list);
-        recyclerView.setAdapter(adapter);
-        recyclerView.addItemDecoration(new TodayListItemDecoration());
-        return view;
-    }
-
 }
