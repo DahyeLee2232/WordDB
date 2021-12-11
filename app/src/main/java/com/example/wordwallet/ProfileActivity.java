38d276f0da833004eef12b3ec477a953ace2f75d
Gyu <rjh7100@gmail.com>
2021-12-11 16:04:39 +0900

    profile update can upload image

--- app/src/main/java/com/example/wordwallet/ProfileActivity.java
+++ app/src/main/java/com/example/wordwallet/ProfileActivity.java
@@ -2,19 +2,26 @@ package com.example.wordwallet;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+
+import com.bumptech.glide.Glide;
 
 import java.io.InputStream;
 
 public class ProfileActivity extends AppCompatActivity {
 
     ImageButton imageButton;
+
+    private static final int REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,7 @@ public class ProfileActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, 1);
+                startActivityForResult(intent, REQUEST_CODE);
             }
         });
     }
@@ -38,15 +45,16 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 1) {
+        if(requestCode == REQUEST_CODE) {
             if(resultCode == RESULT_OK) {
                 try {
-                    InputStream in = getContentResolver().openInputStream(data.getData());
-                    Bitmap img = BitmapFactory.decodeStream(in);
-                    in.close();
+                    Uri uri = data.getData();
+                    Glide.with(getApplication()).load(uri).into(imageButton);
                 } catch (Exception e) {
-                    e.printStackTrace();
+
                 }
+            } else if (resultCode == RESULT_CANCELED) {
+
             }
         }
     }
