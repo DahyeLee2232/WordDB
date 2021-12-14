f35c77d10f0b4c617b40268a104467a3018936e9
DahyeLee2232 <57562540+DahyeLee2232@users.noreply.github.com>
2021-12-14 13:35:48 +0900

    Update PagerFragment.java

--- app/src/main/java/com/example/wordwallet/PagerFragment.java
+++ app/src/main/java/com/example/wordwallet/PagerFragment.java
@@ -10,19 +10,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-/*        View v = convertView;
-
-        if(v == null) {
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = inflater.inflate(R.layout.item_parent_today, parent, false);
-        }
-
-        TextView listName = v.findViewById(R.id.list_name);
-        ImageButton shortcutBtn = v.findViewById(R.id.shortcut_btn);
-        CheckBox bookmark = v.findViewById(R.id.bookmark);
-        ImageButton goBtn = v.findViewById(R.id.arrow_btn);
-
-        listName.setText(getGroup(id).listName);*/
 public class PagerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_one_word, container, false);
